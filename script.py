308b529ddd442e99d7d00b4faabeff4a5cea5ed5
Elton Yael Alba Roldan <22030013@itesa.edu.mx>
2025-10-01 23:32:07 -0600

    EquipoX investigó a elton

--- script.py
+++ script.py
@@ -17,5 +17,5 @@ def info_usuario(usuario):
     }
 
 if __name__ == "__main__":
-    usuario = sys.argv[1] if len(sys.argv) > 1 else "octocat"
+    usuario = sys.argv[1] if len(sys.argv) > 1 else "torvalds"
     print(info_usuario(usuario))
